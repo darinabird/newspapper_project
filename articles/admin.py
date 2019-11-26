7acceb10ced193d21ddf99a76cc00dee35bbf034
Daria Butakhina <darinabird46722@gmail.com>
2019-11-26 13:07:15 +0300

    added comments

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from .models import Article, Comment
 
-from .models import Article
 
-admin.site.register(Article)
+class CommentInline(admin.TabularInline):  # new
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
